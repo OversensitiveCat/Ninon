beefaa091ca9bea08def889f2648026f9c05f3a9
OversensitiveCat <124178833+OversensitiveCat@users.noreply.github.com>
2023-02-25 23:00:23 +0100

    contact mobile

--- src/views/global/set_contact.js
+++ src/views/global/set_contact.js
@@ -8,133 +8,133 @@ import lenis from './lenis'
 gsap.registerPlugin(ScrollTrigger, ScrollToPlugin)
 
 const setContact = (data) => {
-  let mm = gsap.matchMedia()
-  mm.add('(min-width: 992px)', () => {
-    let letters = new SplitType('.contact-heading', {
-      types: 'chars',
-      tagName: 'span',
-    })
-    let copyright = new SplitType('.copyright', {
-      types: 'chars',
-      tagName: 'span',
-    })
-    let tlContact = gsap.timeline({ paused: true })
-    tlContact
-      .to(
-        '.section-contact',
-        {
-          zIndex: 9,
-          duration: 0,
-        },
-        '<'
-      )
-      .to(
-        '.contact-card-container, .nav-bar-fixed, .nav-projets',
-        { backgroundColor: '#141313', duration: 0.4, ease: 'none' },
-        '<'
-      )
-      .from(
-        '.contact-card',
-        {
-          yPercent: 100,
-          duration: 0.8,
-        },
-        '<'
-      )
-      .from(
-        letters.chars,
-        {
-          autoAlpha: 0,
-          scale: 0.2,
-          yPercent: -40,
-          duration: 0.2,
-          stagger: { amount: 0.5 },
-        },
-        '-=0.3'
-      )
-      .from(
-        '.contact-item',
-        {
-          autoAlpha: 0,
-          yPercent: 50,
-          duration: 0.6,
-        },
-        '-=0.4'
-      )
-      .from(
-        '.agence-grid > .link-out, .credits-grid > .contact-subheading',
-        {
-          autoAlpha: 0,
-          yPercent: -60,
-          duration: 0.4,
-        },
-        '-=0.2'
-      )
-      .from(
-        '.agent1 > *',
-        {
-          autoAlpha: 0,
-          yPercent: 40,
-          duration: 0.4,
-          stagger: { amount: 0.5 },
-        },
-        '-=0.2'
-      )
-      .from(
-        '.agent2 > *',
-        {
-          autoAlpha: 0,
-          yPercent: 40,
-          duration: 0.4,
-          stagger: { amount: 0.5 },
-        },
-        '<'
-      )
-      .from(
-        '.credits > *',
-        {
-          autoAlpha: 0,
-          yPercent: 40,
-          duration: 0.4,
-          stagger: { amount: 0.5 },
-        },
-        '<'
-      )
-      .from(
-        copyright.chars,
-        {
-          autoAlpha: 0,
-          stagger: { amount: 0.6 },
-          duration: 0.2,
-        },
-        '-=0.4'
-      )
+  let letters = new SplitType('.contact-heading', {
+    types: 'chars',
+    tagName: 'span',
+  })
+  let copyright = new SplitType('.copyright', {
+    types: 'chars',
+    tagName: 'span',
+  })
+  let tlContact = gsap.timeline({ paused: true })
+  tlContact
+    .to(
+      '.section-contact',
+      {
+        zIndex: 9,
+        duration: 0,
+      },
+      '<'
+    )
+    .to(
+      '.contact-card-container, .nav-bar-fixed, .nav-projets',
+      { backgroundColor: '#141313', duration: 0.4, ease: 'none' },
+      '<'
+    )
+    .from(
+      '.contact-card',
+      {
+        yPercent: 100,
+        duration: 0.8,
+      },
+      '<'
+    )
+    .from(
+      letters.chars,
+      {
+        autoAlpha: 0,
+        scale: 0.2,
+        yPercent: -40,
+        duration: 0.2,
+        stagger: { amount: 0.5 },
+      },
+      '-=0.3'
+    )
+    .from(
+      '.contact-item',
+      {
+        autoAlpha: 0,
+        yPercent: 50,
+        duration: 0.6,
+      },
+      '-=0.4'
+    )
+    .from(
+      '.agence-grid > .link-out, .credits-grid > .contact-subheading',
+      {
+        autoAlpha: 0,
+        yPercent: -60,
+        duration: 0.4,
+      },
+      '-=0.2'
+    )
+    .from(
+      '.agent1 > *',
+      {
+        autoAlpha: 0,
+        yPercent: 40,
+        duration: 0.4,
+        stagger: { amount: 0.5 },
+      },
+      '-=0.2'
+    )
+    .from(
+      '.agent2 > *',
+      {
+        autoAlpha: 0,
+        yPercent: 40,
+        duration: 0.4,
+        stagger: { amount: 0.5 },
+      },
+      '<'
+    )
+    .from(
+      '.credits > *',
+      {
+        autoAlpha: 0,
+        yPercent: 40,
+        duration: 0.4,
+        stagger: { amount: 0.5 },
+      },
+      '<'
+    )
+    .from(
+      copyright.chars,
+      {
+        autoAlpha: 0,
+        stagger: { amount: 0.6 },
+        duration: 0.2,
+      },
+      '-=0.4'
+    )
 
-    let hoverButton = gsap.timeline({ paused: true })
-    hoverButton.to('.close-button', {
-      rotate: 90,
-      duration: 0.3,
-      transformOrigin: 'center',
-      scale: 1.1,
+  let hoverButton = gsap.timeline({ paused: true })
+  hoverButton.to('.close-button', {
+    rotate: 90,
+    duration: 0.3,
+    transformOrigin: 'center',
+    scale: 1.1,
+  })
+
+  let hero = true
+  if (data.next.namespace == 'home') {
+    ScrollTrigger.create({
+      trigger: '.section-hero',
+      start: 'bottom top',
+      onEnter: () => {
+        hero = false
+      },
+      onLeaveBack: () => {
+        hero = true
+      },
     })
+  }
 
-    let hero = true
-    if (data.next.namespace == 'home') {
-      ScrollTrigger.create({
-        trigger: '.section-hero',
-        start: 'bottom top',
-        onEnter: () => {
-          hero = false
-        },
-        onLeaveBack: () => {
-          hero = true
-        },
-      })
-    }
-
-    function set() {
-      const itemsFooter = gsap.utils.toArray('.nav-item-footer')
-      let closeButton = document.querySelector('.close-button')
+  function set() {
+    const itemsFooter = gsap.utils.toArray('.nav-item-footer')
+    let closeButton = document.querySelector('.close-button')
+    let mm = gsap.matchMedia()
+    mm.add('(min-width: 992px)', () => {
       let buttons = [
         itemsFooter[5],
         document.querySelector('.nav-item-contact'),
@@ -167,17 +167,42 @@ const setContact = (data) => {
       closeButton.addEventListener('mouseleave', () => {
         hoverButton.reverse()
       })
+    })
 
-      const links = gsap.utils.toArray('.agent-container > .grey-link')
-      links.forEach((link) => {
-        let tl = gsap.timeline({ paused: true })
-        tl.to(link, { color: '#a80000', duration: 0.1, ease: 'none' })
-        link.addEventListener('mouseenter', () => tl.play())
-        link.addEventListener('mouseleave', () => tl.reverse())
+    mm.add('(max-width: 991px)', () => {
+      let colorNav = gsap.timeline({ paused: true })
+      colorNav.to('.nav-mobile-container', {
+        backgroundColor: '#141313',
+        duration: 0.4,
+        ease: 'none',
       })
-    }
-    return gsap.delayedCall(1, set)
-  })
+      let buttons = [
+        itemsFooter[5],
+        document.querySelector('.nav-text-contact-mobile'),
+      ]
+      buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+          lenis.stop()
+          tlContact.timeScale(1).play()
+          colorNav.timeScale(1).play()
+        })
+      })
+      closeButton.addEventListener('click', () => {
+        lenis.start()
+        tlContact.timeScale(1.8).reverse()
+        colorNav.timeScale(0.6).reverse()
+      })
+    })
+
+    const links = gsap.utils.toArray('.agent-container > .grey-link')
+    links.forEach((link) => {
+      let tl = gsap.timeline({ paused: true })
+      tl.to(link, { color: '#a80000', duration: 0.1, ease: 'none' })
+      link.addEventListener('mouseenter', () => tl.play())
+      link.addEventListener('mouseleave', () => tl.reverse())
+    })
+  }
+  return gsap.delayedCall(1, set)
 }
 
 export default setContact
