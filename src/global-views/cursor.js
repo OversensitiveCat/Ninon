b3232f40dfa25ac736bed069d1ff0f4cb992fc3e
OversensitiveCat <124178833+OversensitiveCat@users.noreply.github.com>
2023-09-20 18:06:32 +0200

    fade in cursor

--- src/global-views/cursor.js
+++ src/global-views/cursor.js
@@ -1,16 +1,25 @@
 import gsap from 'gsap'
 
+import { domLoaded } from '../utilities/loading'
 import { touchDevice } from '../utilities/utilities'
 
 const cursor = () => {
   if (touchDevice()) return
-  gsap.set('.cursor', { display: 'block', xPercent: -50, yPercent: -50 })
+  gsap.set('.cursor', {
+    display: 'block',
+    opacity: 0,
+    xPercent: -50,
+    yPercent: -50,
+  })
   let xTo = gsap.quickTo('.cursor', 'x', { duration: 0.4, ease: 'power3' }),
     yTo = gsap.quickTo('.cursor', 'y', { duration: 0.4, ease: 'power3' })
 
   window.addEventListener('mousemove', (e) => {
     xTo(e.clientX)
     yTo(e.clientY)
+  })
+  domLoaded(function () {
+    gsap.to(cursor, { opacity: 1 })
   })
 }
 
